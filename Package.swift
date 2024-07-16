930cdd61621eb2241071abfa2eef7dba357ef136
Brian Plattenburg <5767019+bplattenburg@users.noreply.github.com>
2024-07-16 11:07:02 -0400

    minimal reproduction

--- Package.swift
+++ Package.swift
@@ -5,17 +5,29 @@ import PackageDescription
 
 let package = Package(
     name: "UnitTestsBrokenWithDittoSDK",
+    platforms: [
+        .macOS("13.0"),
+        .iOS("16.0")
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "UnitTestsBrokenWithDittoSDK",
             targets: ["UnitTestsBrokenWithDittoSDK"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/getditto/DittoSwiftPackage", exact: "4.7.3"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "UnitTestsBrokenWithDittoSDK"),
+            name: "UnitTestsBrokenWithDittoSDK",
+            dependencies: [
+                .product(name: "DittoObjC", package: "DittoSwiftPackage"),
+                .product(name: "DittoSwift", package: "DittoSwiftPackage"),
+            ]
+        ),
         .testTarget(
             name: "UnitTestsBrokenWithDittoSDKTests",
             dependencies: ["UnitTestsBrokenWithDittoSDK"]),
---
+++ README.md
@@ -0,0 +1,1 @@
+An isolated test SPM package to test out why Unit Tests are broken when importing the Ditto SDK
